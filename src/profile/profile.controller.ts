4202be37a4070179579bb8ace94f473a1d2fbf30
Ali Seleem <engaliseleem@gmail.com>
2025-07-15 21:01:38 +0300

    feat: enhance user profile management with update functionality and validation in UpdateUserDto

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,9 +1,9 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Request } from 'express';
-import { AuthService } from 'src/auth/auth.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { ApiResponse } from 'src/common/interfaces/response.interface';
+import { UpdateUserDto } from 'src/users/dto/update-user.dto';
 import { UsersService } from 'src/users/users.service';
 
 @ApiTags('Profile')
@@ -18,10 +18,27 @@ export class ProfileController {
     summary: 'Get user profile',
     description: 'Retrieve the profile information of the authenticated user',
   })
-  async getProfile(@Req() req: Request): Promise<ApiResponse<any>> {
+  async getProfile(@Req() req: Request): Promise<ApiResponse<Express.User>> {
     if (!req.user || typeof req.user['id'] === 'undefined') {
       throw new Error('User information is missing from request.');
     }
     return this.userService.getUserById(req.user['id']);
+  }
+
+  @Patch()
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiOperation({
+    summary: 'Update user profile',
+    description: 'Update the profile information of the authenticated user',
+  })
+  async updateProfile(
+    @Req() req: Request,
+    @Body() body: UpdateUserDto,
+  ): Promise<ApiResponse<Express.User>> {
+    if (!req.user || typeof req.user['id'] === 'undefined') {
+      throw new Error('User information is missing from request.');
+    }
+    return this.userService.updateUser(req.user['id'], body);
   }
 }
--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,12 +1,58 @@
-import { PartialType } from '@nestjs/swagger'; // Changed from @nestjs/mapped-types
-import { CreateUserDto } from './create-user.dto';
-import { ApiPropertyOptional } from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { Gender } from '@prisma/client';
+import {
+  IsEmail,
+  IsIn,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {
+export class UpdateUserDto {
+  @ApiProperty({
+    example: 'John Doe',
+    description: 'Full name of user (2-50 characters, letters only)',
+    minLength: 2,
+    maxLength: 50,
+  })
+  @IsString()
+  @IsNotEmpty({ message: 'Name is required.' })
+  @MinLength(2, { message: 'Name must be at least 2 characters long.' })
+  @MaxLength(50, { message: 'Name must be at most 50 characters long.' })
+  @Matches(/^[A-Za-z\s]+$/, { message: 'Please enter a valid name.' })
+  name: string;
+
+  @ApiProperty({
+    enum: Gender,
+    example: Gender.MALE,
+    description: 'User gender',
+  })
+  @IsString()
+  @IsNotEmpty({ message: 'Gender is required.' })
+  @IsIn(['MALE', 'FEMALE'], {
+    message: 'Gender must be either Male or Female.',
+  })
+  gender: Gender;
+
   @ApiPropertyOptional({
     example: 'Updated Bio',
     description: 'User biography (optional)',
   })
+  @ApiProperty({
+    example: 'Egypt',
+    description: 'User country (2-50 characters, letters only)',
+    minLength: 2,
+    maxLength: 50,
+  })
+  @IsString()
+  @IsNotEmpty({ message: 'Country is required.' })
+  @MinLength(2, { message: 'Country must be at least 2 characters long.' })
+  @MaxLength(50, { message: 'Country must be at most 50 characters long.' })
+  @Matches(/^[A-Za-z\s]+$/, { message: 'Please enter a valid country.' })
+  country: string;
+
   bio?: string;
   // SOCIAL MEDIA (lINKEDIN, BEHANCE, INSTAGRAM, GITHUB)
   @ApiPropertyOptional({
