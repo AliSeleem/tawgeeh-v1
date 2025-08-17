efc00845477b95eb7cb5dbe002b62321ad68c86c
Ali Seleem <engaliseleem@gmail.com>
2025-08-17 22:16:12 +0300

    غيرت رأيي

--- src/main.ts
+++ src/main.ts
@@ -15,11 +15,6 @@ async function bootstrap() {
     abortOnError: false,
   });
 
-  // 👉 Mount backend under /api
-  app.setGlobalPrefix('api', {
-    exclude: [''], // root excluded so "/" still serves frontend
-  });
-  
   // // serve static files
   // const publicPath = path.join(__dirname, '..', 'public');
   // app.useStaticAssets(publicPath);
