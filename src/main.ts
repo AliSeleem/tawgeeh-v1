3fcb89da6b408559609a5ccffa6b1709d75c8d4d
Ali Seleem <engaliseleem@gmail.com>
2025-08-27 01:26:42 +0300

    fix: update Swagger setup path to '/' for correct API documentation access

--- src/main.ts
+++ src/main.ts
@@ -59,7 +59,7 @@ async function bootstrap() {
     .addServer('/api')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('/', app, document);
 
   // listen on port
   await app.listen(process.env.PORT || 3000, '0.0.0.0');
